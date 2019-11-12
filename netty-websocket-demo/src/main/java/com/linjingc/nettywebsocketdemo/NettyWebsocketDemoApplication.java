f907a294ce7b2a1b08f622db66191712c38555f8
chenxiaocehng <583188551@qq.com>
2019-11-12 16:40:25 +0800

    初始化websocket整合netty

---
+++ netty-websocket-demo/src/main/java/com/linjingc/nettywebsocketdemo/NettyWebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.nettywebsocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyWebsocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyWebsocketDemoApplication.class, args);
+	}
+
+}
---
+++ netty-websocket-demo/src/test/java/com/linjingc/nettywebsocketdemo/NettyWebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjingc.nettywebsocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyWebsocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

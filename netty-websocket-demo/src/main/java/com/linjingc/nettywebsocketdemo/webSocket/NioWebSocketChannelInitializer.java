03faa07aec4ce73604a7027f5e698c0ce23f8ae8
chenxiaocehng <583188551@qq.com>
2019-11-13 11:45:19 +0800

    添加websocket的 uri路径

--- netty-websocket-demo/src/main/java/com/linjingc/nettywebsocketdemo/webSocket/NioWebSocketChannelInitializer.java
+++ netty-websocket-demo/src/main/java/com/linjingc/nettywebsocketdemo/webSocket/NioWebSocketChannelInitializer.java
@@ -4,17 +4,25 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class NioWebSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
-    @Override
-    protected void initChannel(SocketChannel ch) {
-        ch.pipeline().addLast("logging",new LoggingHandler("INFO"));//设置log监听器，并且日志级别为debug，方便观察运行流程
-        ch.pipeline().addLast("http-codec",new HttpServerCodec());//设置解码器
-        ch.pipeline().addLast("aggregator",new HttpObjectAggregator(65536));//聚合器，使用websocket会用到
-        ch.pipeline().addLast("http-chunked",new ChunkedWriteHandler());//用于大数据的分区传输
-        ch.pipeline().addLast("handler",new NioWebSocketHandler());//自定义的业务handler
-    }
+	@Override
+	protected void initChannel(SocketChannel ch) {
+		//设置log监听器，并且日志级别为debug，方便观察运行流程
+		ch.pipeline().addLast("logging", new LoggingHandler("INFO"));
+		//设置解码器
+		ch.pipeline().addLast("http-codec", new HttpServerCodec());
+		//聚合器，使用websocket会用到
+		ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
+		//用于大数据的分区传输
+		ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+		//用于处理websocket, /ws为访问websocket时的uri
+		ch.pipeline().addLast(new WebSocketServerProtocolHandler("/ws"));
+		//自定义的业务handler
+		ch.pipeline().addLast("handler", new NioWebSocketHandler());
+	}
 }
 

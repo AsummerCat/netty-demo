19d897f36b45d4fcb485b784abad332c73bd6388
chenxiaocehng <583188551@qq.com>
2019-11-20 10:11:17 +0800

    netty的黏包和分包处理

---
+++ netty-viscous-subpackage-demo/src/main/java/com/linjingc/nettyviscoussubpackagedemo/NettyViscousSubpackageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.linjingc.nettyviscoussubpackagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyViscousSubpackageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyViscousSubpackageDemoApplication.class, args);
+	}
+
+}
---
+++ netty-viscous-subpackage-demo/src/test/java/com/linjingc/nettyviscoussubpackagedemo/NettyViscousSubpackageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjingc.nettyviscoussubpackagedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyViscousSubpackageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
